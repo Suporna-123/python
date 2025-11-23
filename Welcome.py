dffceba3d7a694c8a6137ff9899753a7660094b4
Suporna-123 <dipa.ghosh.3785373@gmail.com>
2025-11-23 18:33:28 +0600

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,8 @@
+print("Welcome to the world of programming")
+print(16)
+print("Hello Python\n")
+print("Keywords in Python r:\n")
+import keyword
+print(keyword.kwlist)
+x=input("Enter your name:")
+print("Welcome!",x)
---
+++ print.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+print("Suporna Ghosh")
+print("I love travelling")
+print("The sum of 3+5:",3+5)
+x=10
+print(x)
+print("The subtraction of 6-2:", 6-2)
